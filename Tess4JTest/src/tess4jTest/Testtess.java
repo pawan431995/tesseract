597b6917c8cc26a1f090c9a59d24f32f1b3150cd
Administrator <Administrator@10.51.27.9>
2019-01-28 16:05:31 +0530

    update

--- Tess4JTest/src/tess4jTest/Testtess.java
+++ Tess4JTest/src/tess4jTest/Testtess.java
@@ -6,13 +6,13 @@ import net.sourceforge.tess4j.*;
 public class Testtess {
 
     public static void main(String[] args) {
-    	System.out.println("\nFile Loading.....\n");
+    	
         File image = new File("C:/bin/Tess4J/songs.png");
-        System.out.println("\nFile Loaded.....\n");
+       
         Tesseract tessInst = new Tesseract();
-        System.out.println("\nCreating Tesseract.....\n");
+        
         tessInst.setDatapath("C://bin//Tess4J");
-        System.out.println("\nsetDatapath.....\n");
+        
         try {
         	System.out.println("Starting ocr.....\n");
             String result= tessInst.doOCR(image);
